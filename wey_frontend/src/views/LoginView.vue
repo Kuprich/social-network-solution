2b16509d57815e4907cc8dc651b88a7888668de9
kuprich <ilya-kyp@yandex.ru>
2024-08-09 22:05:27 +0300

    login page created

--- wey_frontend/src/views/LoginView.vue
+++ wey_frontend/src/views/LoginView.vue
@@ -1,12 +1,8 @@
-<script setup lang="ts">
-import { RouterLink } from "vue-router";
-</script>
-
 <template>
   <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
     <div class="main-left">
       <div class="p-12 bg-white border border-gray-200 rounded-lg">
-        <h1 class="mb-6 text-2xl">Sign up</h1>
+        <h1 class="mb-6 text-2xl">Log in</h1>
 
         <p class="mb-6 text-gray-500">
           Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum
@@ -15,9 +11,8 @@ import { RouterLink } from "vue-router";
         </p>
 
         <p class="font-bold">
-          Already have an account?
-          <RouterLink to="login" class="underline">Click here</RouterLink>
-          to log in!
+          Don't have an account?
+          <RouterLink to="name" class="underline">Click here</RouterLink> to Create one!
         </p>
       </div>
     </div>
@@ -25,14 +20,6 @@ import { RouterLink } from "vue-router";
     <div class="main-right">
       <div class="p-12 bg-white border border-gray-200 rounded-lg">
         <form class="space-y-6">
-          <div>
-            <label>Name</label><br />
-            <input
-              type="text"
-              placeholder="Your full name"
-              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
-            />
-          </div>
 
           <div>
             <label>E-mail</label><br />
@@ -53,19 +40,11 @@ import { RouterLink } from "vue-router";
           </div>
 
           <div>
-            <label>Repeat password</label><br />
-            <input
-              type="password"
-              placeholder="Repeat your password"
-              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
-            />
-          </div>
-
-          <div>
             <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">
-              Sign up
+              Log in
             </button>
           </div>
+
         </form>
       </div>
     </div>
