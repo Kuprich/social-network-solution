80640942369993528244e89495290b5c03cdca5d
kuprich <ilya-kyp@yandex.ru>
2024-08-10 21:08:16 +0300

    Created follow components: PeopleYouMayKnow, Trends

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import SignupView from "../views/SignupView.vue";
-import LoginView from "../views/LoginView.vue";
-import FeedView from "../views/FeedView.vue";
-import MessagesView from "../views/MessagesView.vue";
-import SearchView from "../views/SearchView.vue";
+import HomeView from "@/views/HomeView.vue";
+import SignupView from "@/views/SignupView.vue";
+import LoginView from "@/views/LoginView.vue";
+import FeedView from "@/views/FeedView.vue";
+import MessagesView from "@/views/MessagesView.vue";
+import SearchView from "@/views/SearchView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
