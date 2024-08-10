a0c15a7ab3f2d55462a3984da2b43cbed4b3c1a2
kuprich <ilya-kyp@yandex.ru>
2024-08-10 20:46:09 +0300

    static page search created

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from "../views/HomeView.vue";
 import SignupView from "../views/SignupView.vue";
 import LoginView from "../views/LoginView.vue";
 import FeedView from "../views/FeedView.vue";
+import MessagesView from "../views/MessagesView.vue";
+import SearchView from "../views/SearchView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +23,16 @@ const router = createRouter({
       path: "/feed",
       name: "feed",
       component: FeedView,
+    },
+    {
+      path: "/messages",
+      name: "messages",
+      component: MessagesView,
+    },
+    {
+      path: "/search",
+      name: "search",
+      component: SearchView,
     },
     {
       path: "/login",
